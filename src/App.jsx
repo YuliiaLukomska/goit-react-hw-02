3d1652a60020485cb7aa42ed9eef1069aa6419b5
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-01 20:18:28 +0200

    step 6 was done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import Description from "./components/Description/Description";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [reviews, setReviews] = useState({ good: 0, neutral: 0, bad: 0 });
+  // const [reviews, setReviews] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const [reviews, setReviews] = useState(() => {
+    const savedReviews = localStorage.getItem("reviews");
+    if (savedReviews !== null) {
+      return JSON.parse(savedReviews);
+    } else {
+      return { good: 0, neutral: 0, bad: 0 };
+    }
+  });
+
   const totalFeedback = reviews.good + reviews.neutral + reviews.bad;
   const positiveValue = Math.round(
     ((reviews.good + reviews.neutral) / totalFeedback) * 100
   );
   const updateFeedback = (feedbackType) => {
-    console.log(feedbackType);
     if (feedbackType === "reset") {
       setReviews({ good: 0, neutral: 0, bad: 0 });
     } else {
       setReviews({ ...reviews, [feedbackType]: reviews[feedbackType] + 1 });
     }
-    // Тут використовуй сеттер, щоб оновити стан
   };
 
   const feedbackValuesReset = () => {
@@ -27,6 +35,10 @@ function App() {
       bad: 0,
     });
   };
+
+  useEffect(() => {
+    localStorage.setItem("reviews", JSON.stringify(reviews));
+  }, [reviews]);
 
   return (
     <>
